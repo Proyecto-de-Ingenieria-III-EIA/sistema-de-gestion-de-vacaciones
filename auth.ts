17ddc8c5c987b15497eff604c499c1372c2f3d9d
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2025-01-30 07:38:20 -0500

    feat(env): add .env.example file to provide a template for environment variables fix(gitignore): update .gitignore to exclude .env files for better security feat(auth): integrate Prisma adapter with NextAuth for improved authentication management feat(prisma): create prisma.ts to initialize PrismaClient for database interactions chore(package): add @auth/prisma-adapter and @prisma/client dependencies for authentication and database support

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth';
 import Auth0 from 'next-auth/providers/auth0';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/prisma';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Auth0],
+  adapter: PrismaAdapter(prisma),
 });
