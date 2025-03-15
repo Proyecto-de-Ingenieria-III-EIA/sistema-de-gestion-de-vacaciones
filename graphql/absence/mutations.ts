1e122c7a2d8aea36307999ca63d17f4ed4d3a137
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-03-15 18:27:10 -0500

    Have to add a lot of place holders

--- graphql/absence/mutations.ts
+++ graphql/absence/mutations.ts
@@ -1,7 +1,10 @@
 import gql from "graphql-tag";
 
 const absenceMutations = gql`
-   
+    type Mutation{
+        # Place holder, so the API can run
+        update: Absence
+    }
 `;
 
 export { absenceMutations };
--- graphql/absence/queries.ts
+++ graphql/absence/queries.ts
@@ -1,5 +1,7 @@
 import { gql } from "graphql-tag";
 
+// TODO Add place holder for each mutation and query in all the entities.
+// See the mutations file of absence to see an example of a place holder, this so the API can run
 const absenceQueries = gql`
   type Query {
     
