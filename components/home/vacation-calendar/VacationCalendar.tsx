e48640987731d5b3cf03c1521ca49278cac95a4d
1NVNC1BL3 <isaac.giraldo@eia.edu.co>
2025-03-18 12:20:39 -0500

    Updating footer, header anda vacation calendar

--- components/home/vacation-calendar/VacationCalendar.tsx
+++ components/home/vacation-calendar/VacationCalendar.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import './VacationCalendar.css'
-import { Header } from "./components/Header"
+import Header from "@/components/shared/Header/header"
 import { Content } from "./components/Content"
 import { Card, CardContent } from "../../ui/card"
 import { Conventions } from "./components/Conventions"
@@ -10,7 +10,6 @@ export const VacationCalendar  = () => {
     return (
         <>
             <Card >
-                <Header />
                 <CardContent>
                     <Content />
                     <Conventions />
--- components/shared/Header/header.tsx
+++ components/shared/Header/header.tsx
@@ -1,7 +1,13 @@
-export const Header = () => {
+import React from "react";
+import styles from "./header.module.css";
+
+const Header: React.FC = () => {
     return (
-        <header className='bg-gray-800 text-white p-4'>
-        <h1 className='text-2xl'>Header</h1>
+        <header className={styles.header}>
+            <h1>TimeOff</h1>
+            <img src={"/logoApp.jpg"} alt="Logo de la app" className={styles.logo} />
         </header>
     );
-    }
+};
+
+export default Header;
