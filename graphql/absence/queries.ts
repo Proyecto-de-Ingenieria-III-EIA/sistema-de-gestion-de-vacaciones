69dd87764c129cde6cca4e160e24d4c2003ddedc
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-04 16:59:13 -0500

    absencesTimePeriod from reqAbsence to absence

--- graphql/absence/queries.ts
+++ graphql/absence/queries.ts
@@ -2,6 +2,7 @@ import { gql } from "graphql-tag";
 
 const absenceQueries = gql`
   type Query {
+    getAbsencesTimePeriod(startDate: DateTime!, endDate: DateTime!): [CompleteAbsence]
     getAllAbsences: [Absence]
   }
 `;
--- graphql/requested_absence/queries.ts
+++ graphql/requested_absence/queries.ts
@@ -2,7 +2,6 @@ import { gql } from "graphql-tag";
 
 const requestedAbsenceQueries = gql`
   type Query {
-    getAbsencesTimePeriod(startDate: DateTime!, endDate: DateTime!): [WholeRequestedAbsence]
     getPendingRequestedAbsences: [WholeRequestedAbsence]
   }
 `;
