097f731265ba4781908bfe8f586bad197a2e37e6
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-04 18:57:23 -0500

    Added default vals to status cols

---
+++ prisma/migrations/20250504235615_added_default_values_for_the_status_in_both_req_absen_and_spont_absen/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Requested_Absence" ALTER COLUMN "status" SET DEFAULT 'asdkhqi97udba89d';
+
+-- AlterTable
+ALTER TABLE "Spontaneous_Absence" ALTER COLUMN "status" SET DEFAULT '1231312312';
