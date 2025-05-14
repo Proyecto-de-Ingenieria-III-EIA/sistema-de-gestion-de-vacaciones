3115a9a4f97ace7e710fd4dfcfbc007d8fb260ce
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-13 21:00:48 -0500

    Removed redundadnt column on Justification table

---
+++ prisma/migrations/20250514015957_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `uploaded_at` on the `Justification` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Justification" DROP COLUMN "uploaded_at";
