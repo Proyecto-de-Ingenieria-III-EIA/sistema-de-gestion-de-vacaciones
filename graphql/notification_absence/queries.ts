dd98df751bbef664fca0287fe372a77e8d4c857e
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-15 21:36:03 -0500

    Added a commit

--- graphql/notification_absence/queries.ts
+++ graphql/notification_absence/queries.ts
@@ -2,6 +2,7 @@ import gql from "graphql-tag";
 
 const notificationAbsenceQueries = gql`
     type Query {
+        # TODO make a way for a user (boss or otherwise) to see all its notifications
         getAllNotifications: [Notification_Absence]
     }
 `;
