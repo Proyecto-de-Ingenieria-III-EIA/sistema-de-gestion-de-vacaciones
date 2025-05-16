809588a68adbb0730a712bf06948b9a582134d01
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-15 20:40:15 -0500

    Added the absence field resolver for NotificAbsen

--- graphql/notification_absence/resolvers.ts
+++ graphql/notification_absence/resolvers.ts
@@ -1,5 +1,25 @@
+import { OurContext } from "../context";
+
+interface NotificationAbsence {
+    absenceId: string;
+    isForBoss: boolean;
+    isForWorker: boolean;
+    notificationMessage: string;
+}
+
 const notificationAbsenceResolvers = {
-// TODO add the absence field resolver
+    Notification_Absence: {
+        absence: async (parent: NotificationAbsence, 
+            args: null,
+            context: OurContext
+        ) => {
+            return await context.db.absenceNotification.findFirst({
+                where: {
+                    absenceId: parent.absenceId,
+                }
+            });
+        }
+    }
 };
 
 export { notificationAbsenceResolvers };
