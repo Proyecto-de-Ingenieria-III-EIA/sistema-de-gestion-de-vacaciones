204fd5b1c31e8688dd67a4267c01fd3507a2c7cf
Santiago Yepes Mesa <santiagoyepesmesa0224@gmail.com>
2025-05-20 21:27:12 -0500

    Forgot to remove a TODO

--- graphql/notification_absence/queries.ts
+++ graphql/notification_absence/queries.ts
@@ -2,7 +2,6 @@ import gql from "graphql-tag";
 
 const notificationAbsenceQueries = gql`
     type Query {
-        # TODO make a way for a user (boss or otherwise) to see all its notifications
         getUserNotifications(userId: ID): [Notification_Absence]
     }
 `;
